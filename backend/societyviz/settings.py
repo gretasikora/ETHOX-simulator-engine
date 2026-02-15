200bb3d9263e4a0d98642943889da41aa2557773
Greta Sikora <gretasikora@Gretas-MacBook-Pro.local>
2026-02-15 23:12:15 +0000

    fix

--- backend/societyviz/settings.py
+++ backend/societyviz/settings.py
@@ -98,6 +98,26 @@ if not DEBUG:
     CORS_ALLOWED_ORIGINS = os.environ.get(
         "CORS_ALLOWED_ORIGINS", "http://localhost:5173"
     ).split(",")
+    CORS_ALLOW_CREDENTIALS = True
+    CORS_ALLOW_METHODS = [
+        "DELETE",
+        "GET",
+        "OPTIONS",
+        "PATCH",
+        "POST",
+        "PUT",
+    ]
+    CORS_ALLOW_HEADERS = [
+        "accept",
+        "accept-encoding",
+        "authorization",
+        "content-type",
+        "dnt",
+        "origin",
+        "user-agent",
+        "x-csrftoken",
+        "x-requested-with",
+    ]
 else:
     CORS_ALLOW_ALL_ORIGINS = True
 
